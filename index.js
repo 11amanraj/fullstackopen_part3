23092c970c63ef5e474442456d6cddb8636ca7de
11amanraj <110328977+11amanraj@users.noreply.github.com>
2022-11-24 17:16:31 +0530

    3.15: Phonebook database, step3

--- index.js
+++ index.js
@@ -39,21 +39,29 @@ app.get('/info', (request, response) => {
 })
 
 app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const person = persons.find(person => person.id === id)
-    !person && console.log(false)
-    if (person) {
-      response.json(person)
-    } else {
-      response.status(404).end()
-    }
+    Contact.findById(request.params.id)
+      .then(contact => {
+        if (contact) {
+          response.json(contact)
+        } else {
+          response.status(404).end()
+        }
+      })
+      .catch(error=> {
+        console.log(error)
+        response.status(400).send({ error: 'malformatted id'})
+      })
 })
 
 app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  persons = persons.filter(person => person.id !== id)
-
-  response.status(204).end()
+  Contact.findByIdAndRemove(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => {
+      console.log(error)
+      response.status(404).send({ error: 'deletion error' })
+    })
 })
 
 app.post('/api/persons', (request, response) => {
